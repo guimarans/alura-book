fb5884f28e775c16f16a175271f3610bf0adf751
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-24 14:38:54 -0300

    adicionando github action

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import replace from '@rollup/plugin-replace';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'output',
+    format: 'cjs'
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      __buildDate__: () => JSON.stringify(new Date()),
+      __buildVersion: 15,
+
+      values: {
+        'process.env.DEBUG': 'true'
+      }
+    })
+  ]
+};
